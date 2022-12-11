7395bdedf9d165a1dcda0d2895fd75e7d9127a64
kimad9963 <kimad9963@gmail.com>
2022-12-12 01:01:57 +0500

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatAppUsingSignalR_AspNetMVC.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string username, string message)
+        {
+            Clients.All.SendAsync("RecieveMessage", username,message);
+        }
+    }
+}
